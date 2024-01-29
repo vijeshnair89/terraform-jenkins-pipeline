f783b062e3d12c20f4d9bc77f2415673a39c29f9
Vijesh Vijayan Nair <143416086+vijeshnair89@users.noreply.github.com>
2024-01-29 21:22:34 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,10 +1,10 @@
-variable "aws_access_key" {
+variable "aws_access_key_id" {
   description = "AWS access key"
   type        = string
   default     = ""
 }
 
-variable "aws_secret_key" {
+variable "aws_secret_access_key" {
   description = "AWS secret key"
   type        = string
   default     = ""
