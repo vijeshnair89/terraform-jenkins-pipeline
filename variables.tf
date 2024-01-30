bc67a106b34ef697493d198e3c2a733a7316897b
Vijesh Vijayan Nair <143416086+vijeshnair89@users.noreply.github.com>
2024-01-30 15:28:40 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,20 +1,4 @@
-variable "aws_access_key" {
-  description = "AWS access key"
-  type        = string
-  default     = ""
-}
 
-variable "aws_secret_key" {
-  description = "AWS secret key"
-  type        = string
-  default     = ""
-}
-
-variable "aws_region" {
-  description = "AWS region"
-  type        = string
-  default     = "ap-south-1"
-}
 
 
 variable "ami" {
